a7dcf07ae7011d1aa57edb9b0cf6fae9f3ad0189
Arthur-DevionFox <blackberserkfox@mail.ru>
2023-07-02 12:37:19 +0300

    Внести правку по автотесту. test config

--- controllers/card.js
+++ controllers/card.js
@@ -16,18 +16,18 @@ module.exports.createCard = (req, res) => {
 
 module.exports.deleteCard = (req, res) => {
   Card.findById(req.params._id)
-    .then((card) => res.send({ data: card }))
+    .then((card) => res.send({ }))
     .catch(() => res.status(404).send({ message: 'Такой карточки уже не существует' }));
 };
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
 };
