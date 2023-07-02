99e1c9c74ab6d9231af453771f294170b718c674
Arthur-DevionFox <blackberserkfox@mail.ru>
2023-07-02 12:32:52 +0300

    Внести правку по автотесту. test config

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ mongoose.connect(BASE_PATH)
     console.log(err);
   });
 
-app.use('/users', require('./routes/user'));
-app.use('/cards', require('./routes/card'));
+app.use('/', require('./routes/user'));
+app.use('/', require('./routes/card'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
