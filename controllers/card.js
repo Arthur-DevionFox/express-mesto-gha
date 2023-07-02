53fdd6526611d6df573c3a40e721818eebb50dc3
Arthur-DevionFox <blackberserkfox@mail.ru>
2023-07-02 18:48:00 +0300

    Внести правку по автотесту. test config

--- controllers/card.js
+++ controllers/card.js
@@ -16,18 +16,36 @@ module.exports.createCard = (req, res) => {
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId)
-    .then(() => res.send({ message: 'Карточка удалена' }))
+    .then(() => {
+      if (!req.params.cardId) {
+        res.status(404).send({ message: 'Такой карточки уже не существует' })
+      } else {
+        Card.findByIdAndRemove(req.params.cardId)
+      }
+    })
     .catch(() => res.status(400).send({ message: 'Такой карточки уже не существует' }));
 };
 
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .then(() => res.send({ message: 'Лайк поставлен' }))
-    .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
+    .catch(() => {
+      if (!req.params.cardId) {
+        res.status(404).send({ message: 'Такой карточки не существует' })
+    } else {
+        res.status(400).send({ message: 'Произошла ошибка' })
+      }
+  });
 };
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .then(() => res.send({ message: 'Лайк удален' }))
-    .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
+    .catch(() => {
+      if (!req.params.cardId) {
+        res.status(404).send({ message: 'Такой карточки не существует' })
+      } else {
+        res.status(400).send({ message: 'Произошла ошибка' })
+      }
+    });
 };
